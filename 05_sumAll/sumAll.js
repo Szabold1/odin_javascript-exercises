8bd7e990112310658ef4970c225ca956bdc14145
Boldizsar Szabo <szboldisz@gmail.com>
2022-10-03 21:02:00 +0200

    Add my solutions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  if (num2 > num1) {
+    for (let i = 0; i <= num2 - num1; i++) {
+      sum += num1 + i;
+    }
+  } else if (num1 > num2) {
+    for (let i = 0; i <= num1 - num2; i++) {
+      sum += num1 - i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) / 1.8;
+  if (celsius % 1 !== 0) {
+    return Number(celsius.toFixed(1));
+  } else {
+    return celsius;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let fahrenheit = celsius * 1.8 + 32;
+  if (fahrenheit % 1 !== 0) {
+    return Number(fahrenheit.toFixed(1));
+  } else {
+    return fahrenheit;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
